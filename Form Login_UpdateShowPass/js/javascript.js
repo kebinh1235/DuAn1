a5a07cdecfe5f297d103fdcbba9fe57a3ba415aa
KeBinh <kebinh1235@gmail.com>
2020-04-07 15:31:25 +0700

    Code Responsive Web

--- Form Login_UpdateShowPass/js/javascript.js
+++ Form Login_UpdateShowPass/js/javascript.js
@@ -16,17 +16,17 @@
 
 //Show hoặc ẩn password - Login
     const pass_field_login = document.getElementById("pass_field_login");
-    const show_btn_login= document.getElementById("show_btn_login");
-        show_btn_login.addEventListener("click", function(){
-            if(pass_field_login.type === "password") {
-                pass_field_login.type = "text";
-                show_btn_login.classList.add("hide");
-            } 
-            else {
-                pass_field_login.type = "password";
-                show_btn_login.classList.remove("hide");
-            }
-        });
+    const show_btn_login = document.getElementById("show_btn_login");
+    show_btn_login.addEventListener("click", function(){
+        if(pass_field_login.type === "password") {
+            pass_field_login.type = "text";
+            show_btn_login.classList.add("hide");
+        }
+        else {
+            pass_field_login.type = "password";
+            show_btn_login.classList.add("hide");
+        }
+    });
 //Show hoặc ẩn password - Register
     const pass_field_register = document.getElementById("pass_field_register");
     const show_btn_register= document.getElementById("show_btn_register");
