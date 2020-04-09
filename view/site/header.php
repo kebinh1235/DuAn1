2a3fc624b74e3515a163561720cad55cf3113b15
KeBinh <kebinh1235@gmail.com>
2020-04-09 11:55:38 +0700

    Code Modal-Box

--- view/site/header.php
+++ view/site/header.php
@@ -3,22 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dự án 1</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Quản Lý Nhân Sự</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">  
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<!--Phần mở đầu-->
+
 <body>
-    <!--Phần mở đầu-->
-    <header style="background-position: center; background-size: cover; background-image: url(assets/images/nav_background.jpg);">
+    <!--Start Header-->
+    <header style="background-position: center; background-size: cover; background-image: url(images/banner.jpg);">
         <!--Logo lớn-->
         <div class="heading__box-title">
             <i class="fas fa-users-cog" style="  float: left; color: white; font-size: 60px; padding-top: 30px;"></i>
             <h1>Quản Lý</h1>
             <h2>Nhân Sự</h2>    
-            
             <!--Search-->
             <div class="search">
                 <input type="text" name="#" id="search" placeholder="Nội dung tìm kiếm...">
@@ -44,3 +43,5 @@
             </div>
         </div>
     </nav>
+    <div class="clear"></div>
+    <!--End Header-->
